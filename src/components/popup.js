46481fbfcf970844d8be135321b16cffd414a57e
Firthir <alexfrith@Alexs-MacBook-Pro.local>
2017-11-08 08:23:03 +1000

    popup component

--- src/components/popup.js
+++ src/components/popup.js
@@ -1,39 +1,31 @@
-const popupClassName = 'popup-content'
+import React, { Component } from 'react'
+import styled from 'styled-components'
+const fetch = window.fetch
 
-export default () => {
-
-  const popupButtons = [...document.querySelectorAll('.popup')]
-  if (!popupButtons) return
-
-  const addListeners = (popupElement) => {
-    window.addEventListener('keydown', removePopup)
-    popupElement.addEventListener('click', removePopup)
+class Popup extends Component {
+  state = {
+    title: '',
+    toggle: ''
+  }
+  componentDidMount () {
+    const popup = document.querySelectorAll('.popup')
   }
 
-  const removePopup = (e) => {
-    if (e.type === 'click' && !e.target.classList.contains(popupClassName)) return
-    if (e.type === 'keydown' && e.key !== 'Escape') return
-    const popupElement = document.querySelector(`.${popupClassName}`)
-    popupElement.remove()
-    window.removeEventListener('keydown', removePopup)
-    document.body.classList.remove('popup-open')
+  handleChange = (e) => {
+    this.setState({
+      [e.target.toggle]: 'open'
+    })
   }
 
-  const openPopup = function(e) {
+  openPupup = (e) => {
     e.preventDefault()
-    const html = `
-      <div class="popup-content container">
-        <img src="${this.href}" />
-      </div>
-    `
-    const popup = document.createElement('div')
-    popup.classList.add(popupClassName)
-    popup.innerHTML = html
-    document.body.appendChild(popup)
-    document.body.classList.add('popup-open')
-    addListeners(popup)
   }
 
-  popupButtons.forEach(btn => btn.addEventListener('click', openPopup))
+  render ({title = 'click here', toggle}) {
+    return (
 
+    )
+  }
 }
+
+export default Popup
